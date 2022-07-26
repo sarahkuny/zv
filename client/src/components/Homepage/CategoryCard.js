fc91e4bf6122ea7e22fa20b2a1cc42ad11bfbe2f
Sarah Kuny <sarahkuny@icloud.com>
2022-07-25 19:28:46 -0500

    mobile view margins fixed

--- client/src/components/Homepage/CategoryCard.js
+++ client/src/components/Homepage/CategoryCard.js
@@ -1,12 +1,12 @@
 
 function CategoryCard({ category }) {
     return ( 
-        <div className="flex flex-col-reverse lg:flex-row items-center justify-evenly md:border-dotted md:border-4 rounded-lg md:border-white w-full md:w-2/3 lg:h-72 my-10 py-10 cursor-pointer snap-center hover:bg-[#2644FD] drop-shadow-[0px_0px_20px_rgba(255,255,255,0.25)]">
+        <div className="flex flex-col-reverse lg:flex-row items-center justify-evenly md:border-dotted md:border-4 rounded-lg md:border-white w-full md:w-2/3 lg:h-96 my-10 py-10 cursor-pointer snap-center hover:bg-[#2644FD] drop-shadow-[0px_0px_20px_rgba(255,255,255,0.25)]">
             <div className="flex flex-col w-1/2 items-center justify-center pl-4">
-                <h2 className="text-8xl font-Arcade">{category.title}</h2>
+                <h2 className=" text-6xl md:text-8xl font-Arcade">{category.title}</h2>
                 <h5 className="text-3xl text-center">{category.description}</h5>
             </div>
-            <img src={category.image} className="object-scale-down  h-48 rounded-lg"/>
+            <img src={category.image} alt={category.title} className="object-scale-down  h-48 rounded-lg"/>
         </div>
      );
 }
--- client/src/components/Shop/Shop.js
+++ client/src/components/Shop/Shop.js
@@ -9,7 +9,7 @@ function Shop() {
     return (
         <div className="bg-[#2644FD] cursor-pointer h-full">
             <div className="flex flex-col items-center ">
-                <h1 className="text-white font-Arcade text-8xl text-center mt-2 pb-4 px-8 border-4 border-dotted rounded-md">Shop Items</h1>
+                <h1 className="text-white font-Arcade text-8xl text-center mt-2 pb-4 px-8 mx-2 border-4 border-dotted rounded-md">Shop Items</h1>
                 <div className="  flex flex-col my-10 p-2 md:w-5/6 gap-4 md:grid md:grid-cols-2 lg:grid-cols-4 md:gap-6">
                     {pokemon.map((item) => <ProductCard product={item} />)}
                 </div>
