657a859e4273bbd118b2fa5d2fef2dc10e935546
Sarah Kuny <sarahkuny@icloud.com>
2022-07-25 19:36:10 -0500

    set add to cart button width

--- client/src/components/Shop/AddToCart.js
+++ client/src/components/Shop/AddToCart.js
@@ -2,7 +2,7 @@ import { Icon } from '@iconify/react';
 
 function AddToCart() {
     return ( 
-        <button className="bg-neutral-900 text-white px-2 py-1 rounded flex justify-center items-center text-md hover:bg-[#2644FD]">
+        <button className="bg-neutral-900 w-36 text-white px-2 py-1 rounded flex justify-center items-center text-md hover:bg-[#2644FD]">
             <Icon icon="dashicons:cart"  className="mr-2" /> Add to Cart
         </button>
      );
